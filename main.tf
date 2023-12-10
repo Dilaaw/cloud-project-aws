d711c3d24627e99e6fbe7aef429b72f062148e60
Walid Rahioui <john.doe@etu.imt-nord-europe.fr>
2023-12-10 16:20:58 +0100

    init project

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      group = "echo"
+    }
+  }
+}
+
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.19"
+
+    }
+  }
+
+  backend "s3" {
+    bucket = "echo-s3-web"
+    key    = "tf/tfstate"
+    region = "eu-west-1"
+  }
+}
+
+resource "aws_s3_bucket" "echo_s3_bucket" {
+  bucket = "echo-s3-web"
+
+  tags = {
+    Name  = "echo-s3-bucket"
+    Group = "echo"
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam-echo-lambda" {
+  name               = "iam-echo-lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
